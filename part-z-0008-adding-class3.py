946cdfbf6ba3f1ee95354f4521ab7e7bf4e6b115
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-13 09:48:16 +0300

    Create part-z-0008-adding-class3.py

---
+++ part-z-0008-adding-class3.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class AREA:
+    data_id: str
+    area: str
+    lang: str
+    liked: bool
+    average_salary: int
+
+    def __init__(self, data_id, area, lang, liked, average_salary):
+
+        self.data_id = data_id
+        self.area = area
+        self.lang = lang
+        self.liked = liked
+        self.average_salary = average_salary
+
+AREAS = [
+    AREA("d01", "AI", "Python", True, 74_000),
+    AREA("d02", "infrastructure", "Rust", True, 99_000),
+    AREA("d03", "Smart Contracts", "Solidity", False, 77_000),
+    AREA("d04", "Web", "JS", False, 65_000),
+    AREA("d05", "Mobile", "Kotlin", True, 87_000),
+    AREA("d06", "Mobile", "JS", False, 70_000),
+    AREA("d07", "Smart Contracts", "Vyper", True, 84_000),
+    AREA("d08", "Smart Contracts", "Move", True, 92_000),
+]
+
+AREAS.append(AREA("d09", "infrastructure", "Go", True, 68_000))
+
+@app.get("/get-areas")
+def get_areas():
+
+    return AREAS
+
+# uv run uvicorn main:app --reload
